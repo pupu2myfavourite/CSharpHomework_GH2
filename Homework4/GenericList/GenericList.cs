0d01257b98fc6ed9ac3d5d3ef87c687de8e4f1d8
XiaoPi2 <1484326102@qq.com>
2020-03-11 22:37:50 +0800

    This is Homework4

---
+++ Homework4/GenericList/GenericList.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Homework4
+{
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+        public int length; 
+        public GenericList()
+        {
+            tail = head = null;
+            length = 0;
+        }
+        public Node<T> Head
+        {
+            get => head;
+        }
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if(tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+            length++;
+        }
+        public void forEach(Action<T> action) {
+            Node<T> temp = head;            
+            while(temp != null)
+            {
+                action(temp.Data);
+                temp = temp.Next;
+            }
+            Console.WriteLine();
+        }
+     
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericList<int> list = new GenericList<int>();
+            for(int i = 0; i < 10; i++)
+            {
+                list.Add(i);
+            }
+
+            Console.WriteLine("该范型list的值为：");
+            list.forEach(x =>
+            {
+                Console.Write(x + "\t");
+            });
+            int sum = 0;
+            list.forEach(x => { 
+                sum += x;
+            });
+            Console.WriteLine("和：{0}", sum);
+            int max = int.MinValue;
+            list.forEach(x =>
+            {
+                max = max < x ? x : max;
+            });
+            Console.WriteLine("最大值：{0}", max);
+            int min = int.MaxValue;
+            list.forEach(x =>
+            {
+                min = min > x ? x : min;
+            });
+            Console.WriteLine("最小值：{0}", min);
+        }
+    }
+}
---
+++ Homework4/clock/Clock.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Threading;
+
+namespace clock
+{
+    //声明一个委托类型，定义事件处理函数的格式
+    public delegate void ClockEventHandler(object sender, TimeEventArgs args);
+
+    public class TimeEventArgs
+    {
+        public int Hour { get; set; }
+        public int Min { get; set; }
+        public int Sec { get; set; }
+    }
+
+    public class Clock
+    {
+        //定义时间，相当于创建一个委托实例
+        public event ClockEventHandler AlarmEvent;
+        public event ClockEventHandler TickEvent;
+
+        public void setTime(TimeEventArgs args)
+        {
+            args.Hour = DateTime.Now.Hour;
+            args.Min = DateTime.Now.Minute;
+            args.Sec = DateTime.Now.Second;
+        }
+
+        public void Work(int h, int m, int s)
+        {
+            Console.WriteLine($"Right now is {h}:{m}:{s}. Start from here:");
+            TimeEventArgs args = new TimeEventArgs()
+            {
+                Hour = h,
+                Min = m,
+                Sec = s
+            };
+            while (true)
+            {
+                setTime(args);
+                //TickEvent(this, args);
+                if (args.Sec %5 == 0)
+                {
+                    AlarmEvent(this, args);
+                }
+                else
+                {
+                    TickEvent(this, args);
+                }
+                Thread.Sleep(1000);
+            }
+        }
+    }
+    public class Test
+    {
+        public Clock clock = new Clock();
+        public Test()
+        {
+            //为clock的两个事件添加方法
+            clock.AlarmEvent += new ClockEventHandler(ClockAlarm);
+            clock.TickEvent += new ClockEventHandler(ClockTick);
+        }
+        void ClockAlarm(object sender,TimeEventArgs args)
+        {
+            Console.WriteLine($"Now is {args.Hour}:{args.Min}:{args.Sec}");
+        }
+        void ClockTick(object sender,TimeEventArgs args)
+        {
+            Console.WriteLine($"{args.Hour}:{args.Min}:{args.Sec}");
+        }
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test test = new Test();
+            test.clock.Work(22, 22, 22);//设定开始事件并开始
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
