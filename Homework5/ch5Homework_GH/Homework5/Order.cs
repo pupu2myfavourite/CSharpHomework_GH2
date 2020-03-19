76154a1b23d37f69b303bdefe364c2aad7438485
XiaoPi2 <1484326102@qq.com>
2020-03-19 09:32:15 +0800

    This is Homework5 By GaoHang

---
+++ Homework5/ch5Homework_GH/Homework5/Order.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace OrderManagement
+{
+    public class Order : IComparable
+    {
+        public int OrderID { get; set; }
+        public int ProductID { get; set; }
+        public int ClientID { get; set; }
+
+        public OrderItem orderItem;
+        public Client client;
+
+        public Order()
+        {
+        }
+        public Order(int orderID, Client newClient, OrderItem newOrderItem)
+        {
+            OrderID = orderID;
+            ClientID = newClient.ClientID;
+            ProductID = newOrderItem.ProductID;
+
+            client = newClient;
+            orderItem = newOrderItem;
+        }
+        public override string ToString()
+        {
+
+            //这个被注释的方法会报错，暂时没解决
+            //return "OrderID:"+OrderID +" -OrderItem:"+orderItem.ToString()
+            //+"-Client:"+client.ToString();
+            return "orderID:" + OrderID + " -ProductID:" + orderItem.ProductID; //+
+                //" -ProductName"+orderItem.product.ProductName;
+        }
+
+        public int CompareTo(object obj)
+        {
+            Order otherOrder = obj as Order;
+            if (this.OrderID > otherOrder.OrderID)
+            {
+                return 1;
+            }
+            else if (this.OrderID == otherOrder.OrderID)
+            {
+                return 0;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+
+            if (this.CompareTo(obj) != 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }       
+    }
+}
---
+++ Homework5/ch5Homework_GH/Homework5/OrderService.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OrderManagement
+{
+    class OrderService
+    {
+        static List<Order> OrderList = new List<Order> { };
+
+        //四种业务：添加、删除、修改、查询订单
+        //添加订单
+        public static bool CreateOrder(Order order)
+        {
+            foreach(Order o in OrderList)
+            {
+                if (o.Equals(order))
+                {
+                    Console.WriteLine("创建失败，订单已存在!  OrderID:"+ order.OrderID);
+                    return false;
+                }
+            }
+            //Order order = new Order(OrderID, newClient, newOrderItem);
+            OrderList.Add(order);
+            Console.WriteLine("添加订单成功！ OrderID:"+order.OrderID );
+            return true;
+        }
+
+        //查询订单
+        public static bool QueryOrder(int orderID,out IEnumerable<Order> orders)
+        {
+            //可以用OrderBy来对订单进行排序
+            IEnumerable<Order> order = from o in OrderList
+                                       where o.OrderID == orderID
+                                       orderby o.orderItem.TotalMoney ascending
+                                       select o;          
+            if (order.Count() > 0)
+            {
+                orders = order;
+                return true;
+            }
+            else
+            {
+                orders = null;
+                return false;
+            }
+            //Console.WriteLine("查询结果:");
+            //foreach(var o in order)
+            //{
+            //    Console.WriteLine(o.ToString());
+            //}
+
+        }
+
+        //修改订单
+        public static bool UpdateOrder(int orderID,int ProductID)
+        {
+            IEnumerable<Order> order = from o in OrderList
+                          where o.OrderID == orderID
+                          select o;
+            if(order.Count() > 0)//查询到有此订单
+            {
+                foreach (var o in OrderList)//找到OrderList中的此订单，进行修改
+                {
+                    if(o.OrderID == orderID)
+                    {
+                        o.ProductID = ProductID;
+                    }
+
+                }
+                //Console.WriteLine("修改成功！");
+                return true;
+            }
+            else//未查询到订单
+            {
+                //Console.WriteLine("修改失败，未查询到订单。");
+                return false;
+            }
+             
+        }
+
+        //删除订单
+        public static bool DeleteOrder(int orderID)
+        {
+            IEnumerable<Order> order = from o in OrderList
+                                       where o.OrderID == orderID
+                                       select o;
+            
+            if(order.Count() > 0)//查询到由此订单
+            {
+                foreach (var o in OrderList)
+                {
+                    if (o.OrderID == orderID)
+                    {
+                        //OrderList.Append(o);
+                        OrderList.Remove(o);
+                        break;
+                    }
+                }
+                Console.WriteLine("删除成功！");
+                return true;
+            }
+            else//微查询到此订单
+            {
+                Console.WriteLine("删除失败，未查询到订单。");
+                return false;
+            }
+        }
+
+        //对订单排序
+        public static void Sort()
+        {
+            OrderList.Sort();
+        }
+
+        //遍历订单
+        public static void Traversing()
+        {
+            foreach(Order o in OrderList)
+            {
+                Console.WriteLine(o.ToString());
+            }
+        }
+    }
+}
