0a3f191188178b282e0547896af7f2145f5ca5b6
XiaoPi2 <1484326102@qq.com>
2020-03-30 16:22:54 +0800

    This is Homework7

---
+++ Homework7-3.30/ch7Homework_GH/ch7Homework_GH/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace ch7Homework_GH
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.draw = new System.Windows.Forms.Button();
+            this.colors = new System.Windows.Forms.ComboBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // draw
+            // 
+            this.draw.Location = new System.Drawing.Point(57, 15);
+            this.draw.Name = "draw";
+            this.draw.Size = new System.Drawing.Size(75, 23);
+            this.draw.TabIndex = 0;
+            this.draw.Text = "draw";
+            this.draw.UseVisualStyleBackColor = true;
+            this.draw.Click += new System.EventHandler(this.draw_Click);
+            // 
+            // colors
+            // 
+            this.colors.FormattingEnabled = true;
+            this.colors.Items.AddRange(new object[] {
+            "Red",
+            "Green",
+            "Blue"});
+            this.colors.Location = new System.Drawing.Point(57, 81);
+            this.colors.Name = "colors";
+            this.colors.Size = new System.Drawing.Size(75, 20);
+            this.colors.TabIndex = 1;
+            this.colors.Text = "   COLOR";
+            this.colors.SelectedIndexChanged += new System.EventHandler(this.colors_SelectedIndexChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(496, 506);
+            this.panel1.TabIndex = 2;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(46, 144);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.TabIndex = 3;
+            this.trackBar1.Value = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 153);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Width";
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(46, 230);
+            this.trackBar2.Maximum = 7;
+            this.trackBar2.Minimum = 1;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(104, 45);
+            this.trackBar2.TabIndex = 5;
+            this.trackBar2.Value = 1;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 235);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "height";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(156, 230);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(32, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.trackBar3);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.draw);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.colors);
+            this.panel2.Controls.Add(this.trackBar2);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.trackBar1);
+            this.panel2.Location = new System.Drawing.Point(525, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 506);
+            this.panel2.TabIndex = 8;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(158, 307);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(30, 23);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "10";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(5, 312);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 12);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Speed";
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(46, 303);
+            this.trackBar3.Minimum = 1;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(104, 45);
+            this.trackBar3.TabIndex = 10;
+            this.trackBar3.Value = 10;
+            this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(117, 494);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 12);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "design by GH2";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(154, 147);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(34, 25);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "1";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(737, 531);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "CayleyTree";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button draw;
+        private System.Windows.Forms.ComboBox colors;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TrackBar trackBar3;
+    }
+}
+
---
+++ Homework7-3.30/ch7Homework_GH/ch7Homework_GH/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ch7Homework_GH
+{
+    public partial class Form1 : Form
+    {
+
+        private Graphics graphics;
+        double th1 = 30 * Math.PI / 180;
+        double th2 = 20 * Math.PI / 180;
+        double per1 = 0.6;
+        double per2 = 0.7;
+        Color color = Color.Green;
+        int width = 1;
+        double leng = 100;
+        int drawInterval = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void draw_Click(object sender, EventArgs e)
+        {
+            if (graphics == null) graphics = this.panel1.CreateGraphics();
+            graphics.Clear(Color.White);
+            drawCayleyTree(14, panel1.Width/2, panel1.Height/1.05, leng, -Math.PI / 2);
+        }
+
+        void drawCayleyTree(int n, double x0,double y0,double leng, double th)
+        {
+            if (n == 0) return;
+
+            timer1.Start();
+
+            double x1 = x0 + leng * Math.Cos(th);
+            double y1 = y0 + leng * Math.Sin(th);
+
+            drawLine(x0, y0, x1, y1,width);
+
+            timer1.Stop();
+
+            drawCayleyTree(n - 1, x1, y1, per1 * leng, th + th1);
+            drawCayleyTree(n - 1, x1, y1, per2 * leng, th - th2);
+
+        }
+        void drawLine(double x0,double y0,double x1,double y1,int width)
+        {
+            graphics.DrawLine(
+                new Pen(color,width),
+                (int)x0, (int)y0, (int)x1, (int)y1);
+        }
+
+        private void colors_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string SelectedColor = colors.SelectedItem.ToString();
+            switch (SelectedColor)
+            {
+                case "Red":
+                    color = Color.Red;                    
+                    break;
+                case "Blue":
+                    color = Color.Blue;
+                    break;
+                case "Green":
+                    color = Color.Green;
+                    break;
+                default:
+                    break;
+            }
+            
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+       
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            button2.Text = trackBar1.Value.ToString();
+            width = trackBar1.Value;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            button1.Text = trackBar2.Value.ToString();
+            leng = (1 + (double)trackBar2.Value / 10) * 100;
+        }
+
+        private void trackBar3_Scroll(object sender, EventArgs e)
+        {
+            button3.Text = trackBar3.Value.ToString();
+            drawInterval = 1100 - trackBar3.Value * 100;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            timer1.Interval = drawInterval;
+            //在drawCayleyTree中调用start和stop，但是有问题，未解决
+        }
+    }
+}
